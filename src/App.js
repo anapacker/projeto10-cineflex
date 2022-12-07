1e94960fd6cfcba257d0056fbd7391b0bc74c706
Ana Packer <anaamaria.packer@gmail.com>
2022-12-07 15:19:48 -0300

    layout finalizado

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ function App() {
         <h1>CINEFLEX</h1>
       </ScreenHeader>
 
+      <TextCoontainer>
+        <p>Selecione o filme</p>
+
+      </TextCoontainer>
+
       <ScreenContainer>
+
         <Movies />
         <Movies />
         <Movies />
@@ -35,9 +41,27 @@ const ScreenHeader = styled.div`
       font-size:34px;
       font-Weigth:400;
       color:#E8833A;
+      padding: 20px 0 20px 0;
     }
 `
 
 const ScreenContainer = styled.div`
-    min-width:100%;
+    min-width:100vw;
+    min-height: 100vh;
+    align-items: center;
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    
+    `
+const TextCoontainer = styled.p`
+    font-size:24px;
+    font-family: 'Roboto';
+    padding: 20px 0 20px 0;
+    display: flex;
+    justify-content: center ;
+    align-items: center;
+
+      
 `
+
--- src/Movies.js
+++ src/Movies.js
@@ -1,5 +1,37 @@
+import styled from "styled-components"
+import imgMovie from "../src/image3.png"
+
 export default function Movies() {
     return (
-        <div>Movies</div>
+        <>
+            <div>
+
+                <BoxMovies>
+                    <img src={imgMovie} alt="imgMovie" />
+                </BoxMovies>
+
+            </div>
+        </>
+
     )
 }
+
+
+
+
+
+const BoxMovies = styled.div`
+    background-color: blue;
+    width: 145px;
+    height: 209px;
+    display: flex;
+    justify-content: center;
+    margin-top:15px;
+    background-color:white;
+    box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
+    border-radius: 3px;
+        img{
+            width: 140px;
+            padding: 8px;
+        }
+`
