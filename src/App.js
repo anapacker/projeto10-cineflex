9e7424be339c8d1aac179f7db973f5afe2185fc2
Ana Packer <anaamaria.packer@gmail.com>
2022-12-06 23:08:37 -0300

    iniciando componentização do app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import styled from "styled-components";
+import Movies from "./Movies";
+
 function App() {
   return (
     <>
-      Cineflex
+      <ScreenHeader>
+        <h1>CINEFLEX</h1>
+      </ScreenHeader>
+
+      <ScreenContainer>
+        <Movies />
+        <Movies />
+        <Movies />
+        <Movies />
+        <Movies />
+
+      </ScreenContainer>
+
     </>
   );
 }
 
+
+
 export default App;
+
+
+const ScreenHeader = styled.div`
+  background-color:#C3CFD9;
+  display:flex;
+  justify-content: center;
+    h1{
+      font-family:'Roboto';
+      font-size:34px;
+      font-Weigth:400;
+      color:#E8833A;
+    }
+`
+
+const ScreenContainer = styled.div`
+    min-width:100%;
+`
